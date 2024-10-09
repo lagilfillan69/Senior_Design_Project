d7d03ec37faa17bfcafbc8ceb1b58d416c215a6d
Lauren Gilfillan <lag179@pitt.edu>
2024-10-09 17:18:54 +0200

    work on dockerimage with stuff

---
+++ Docker_Image/ros_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+ros_env_setup="/opt/ros/$ROS_DISTRO/setup.bash"
+catkin_env_setup="$WORKSPACE/install/setup.bash"
+
+echo "sourcing   $ros_env_setup"
+source "$ros_env_setup"
+
+echo "sourcing   $catkin_env_setup"
+source "$catkin_env_setup"
+
+echo "ROS_ROOT   $ROS_ROOT"
+echo "ROS_DISTRO $ROS_DISTRO"
+
+exec "$@"
