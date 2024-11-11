e790c445c05c85574c53a78a78bf2f441e0e4d10
Lauren Gilfillan <lag179@pitt.edu>
2024-11-10 21:45:49 -0500

    added arduino stuff

--- camera_ros_node/build/multisense_msgs/ament_cmake_python/multisense_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='multisense_msgs',
-    version='1.0.0',
-    packages=find_packages(
-        include=('multisense_msgs', 'multisense_msgs.*')),
-)
