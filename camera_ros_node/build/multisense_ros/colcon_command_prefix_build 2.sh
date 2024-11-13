6d08b1e4bb31c573bc2060aecfdf0f7df54e5514
Lauren Gilfillan <lag179@pitt.edu>
2024-11-13 10:09:52 -0500

    Jetson Commit

--- camera_ros_node/build/multisense_ros/colcon_command_prefix_build 2.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jetson/Senior_Design_Project/camera_ros_node/install/multisense_lib/share/multisense_lib/package.sh"
-. "/home/jetson/Senior_Design_Project/camera_ros_node/install/multisense_msgs/share/multisense_msgs/package.sh"
